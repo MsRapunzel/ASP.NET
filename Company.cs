72fa55420613832a70744678400b4df6ed783564
MsRapunzel <anastasya.zolotova11@gmail.com>
2024-02-13 11:13:02 +0200

    basic project, middleware

---
+++ Company.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ASP.NET
+{
+    public class Company
+    {
+        public string? Name { get; set; }
+        public string? HeadquartersAddress { get; set; }
+        public int? FoundedYear { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,35 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using ASP.NET;
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+Company company = new Company { Name = "Apple Inc.", HeadquartersAddress = "1 Apple Park Way, Cupertino, California, U.S.", FoundedYear = 1976 };
+
+app.MapGet("/task1", () =>
+{
+    return $"Company name: \"{company.Name}\"," +
+    $" its headquarters address: \"{company.HeadquartersAddress}\"," +
+    $" and its foundation year: \"{company.FoundedYear}\".";
+});
+
+app.MapGet("/task2", () =>
+{
+    return $"Random number from 1 to 100 is: {new Random().Next(0, 101)}";
+});
+
+app.Use(async (context, next) =>
+{
+    var token = context.Request.Query["token"];
+    if (token != "12345678")
+    {
+        context.Response.StatusCode = 403;
+        await context.Response.WriteAsync("Token is invalid");
+    }
+    else
+    {
+        await next.Invoke(context);
+    }
+
+});
 
 app.Run();
-
