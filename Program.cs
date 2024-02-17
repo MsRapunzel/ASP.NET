c4e9ddbe09a054525bbeb2e2d95b889006a21f9f
MsRapunzel <anastasya.zolotova11@gmail.com>
2024-02-17 17:31:58 +0200

    lab work 5 – add logging

--- Program.cs
+++ Program.cs
@@ -1,58 +1,108 @@
-﻿using labWork.Models;
+﻿using labWork.Logging;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder();
-
-builder.Services.AddTransient<Book>();
-builder.Services.AddTransient<User>();
-
-builder.Configuration.
-    AddJsonFile("Configuration/Books.json").
-    AddJsonFile("Configuration/Users.json");
+WebApplicationBuilder builder = WebApplication.CreateBuilder();
+builder.Logging.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "log.txt"));
 
 var app = builder.Build();
+var logger = app.Logger;
 
-app.Map("/Library", () => "Greetings!");
 
-app.Map("/Library/Books", (HttpContext context, IConfiguration config) =>
+app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-    context.Response.ContentType = "text/html; charset=utf-8";
-    var stringBuilder = new System.Text.StringBuilder("<table style=\"width:450px;\">");
-
-    var books = config.GetSection("library:books").Get<Book[]>();
-
-    stringBuilder.Append("" +
-        "<tr>" +
-            "<th>Title</th>" +
-            "<th>Author</th>" +
-            "<th>Date Of Publishing</th>" +
-        "</tr>");
-
-    foreach (var item in books)
+    try
     {
-        stringBuilder.Append("" +
-            "<tr>" +
-                $"<td>{item.Title}</td>" +
-                $"<td>{item.Author}</td>" +
-                $"<td>{item.DateOfRelease}</td>" +
-            "</tr>");
+        await next.Invoke(context);
     }
+    catch (Exception ex)
+    {
+        var log = new StringBuilder();
 
-    stringBuilder.Append("</table>");
+        log.AppendLine($"\nTime:\t\t\t\t\t{DateTime.Now.ToString()}");
+        log.AppendLine($"Request Path:\t\t\t{context.Request.Path}");
+        log.AppendLine($"Request Method:\t\t\t{context.Request.Method}");
 
-    return $"{stringBuilder}";
+        if (context.Request.HasFormContentType && context.Request.Form.Any())
+        {
+            foreach (var form in context.Request.Form)
+            {
+                log.AppendLine($"Form Data: {form.Key} = {form.Value}");
+            }
+        }
+
+        log.AppendLine($"{ex}");
+
+        logger.LogError(message: log.ToString());
+
+        throw;
+    }
 });
 
-app.Map("/Library/Profile/{id:range(0,5):int?}", (int? id, IConfiguration config) =>
+app.MapGet("/", async (context) =>
 {
-    if (!id.HasValue)
+    var htmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "index.html");
+
+    if (File.Exists(htmlFilePath))
     {
-        return $"There is no user, you did not add user id.";
+        await context.Response.WriteAsync(File.ReadAllText(htmlFilePath));
+    }
+    else
+    {
+        await context.Response.WriteAsync("<p>File's path is wrong.</p>");
+        throw new Exception("File's path is wrong.");
+    }
+});
+
+app.MapPost("/set-cookies", async context =>
+{
+    var value = context.Request.Form["value"];
+    var expiration = context.Request.Form["expiration"];
+
+    if (DateTime.Parse(expiration) < DateTime.Now)
+    {
+        await context.Response.WriteAsync(
+            $"<p>Value \"{value}\" is expired.</p>" +
+            $"<a href='/'>Try again.</a>");
+
+        throw new Exception($"Value \"{value}\" is expired.");
     }
 
-    var users = config.GetSection("profile:users").Get<User[]>();
-    var user = users.FirstOrDefault(user => user.Id == id);
+    if (DateTime.TryParse(expiration, out DateTime _expiration))
+    {
+        context.Response.Cookies.Append(
+            "MyCookie",
+            value,
+            new CookieOptions
+            {
+                Expires = _expiration
+            });
 
-    return $"User is {user.Name}";
+        await context.Response.WriteAsync(
+            "<p>Value is in Cookies. "
+            + "<a href='/check-cookies/'>Check Cookie</a></p>");
+    }
+
+    else
+    {
+        await context.Response.WriteAsync("<p>Error with expiration date." +
+            "<a href='/'>Try again.</p>");
+        throw new Exception("Error with expiration date");
+    }
+});
+
+app.MapGet("/check-cookies", async context =>
+{
+    var cookie = context.Request.Cookies["MyCookie"];
+
+    if (!string.IsNullOrEmpty(cookie))
+    {
+        await context.Response.WriteAsync($"<p>Value: {cookie}</p>");
+    }
+    else
+    {
+        await context.Response.WriteAsync("<p>There is no value in Cookies</p>");
+        throw new Exception("There is no value in Cookies");
+    }
 });
 
 app.Run();
---
+++ Providers/FileLoggerProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using labWork.Logging;
+
+namespace labWork.Providers
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        string path;
+        public FileLoggerProvider(string path)
+        {
+            this.path = path;
+        }
+        public ILogger CreateLogger(string categoryName) => new FileLogger(path);
+
+        public void Dispose() { }
+    }
+}
+
